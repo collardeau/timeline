959318771dad4c86e0a11d4ecb408d3ca1f3f258
Thomas Collardeau <thomas@collardeau.com>
2015-05-26 19:33:55 +0200

    add ratchet components

--- app/components/browse/Browse.js
+++ app/components/browse/Browse.js
@@ -13,10 +13,45 @@ class Browse extends React.Component {
     return (
       <div>
         <header className="bar bar-nav">
+          <button className="btn pull-left">
+            <i className="fa fa-bars"></i>
+          </button>
           <h1 className="title">
             Browse
           </h1>
+          <button className="btn pull-right">
+            <i className="fa fa-plus"></i>
+          </button>
         </header>
+
+        <div className="content">
+
+          <div className="segmented-control">
+            <a className="control-item active">Only Public</a>
+            <a className="control-item">Only Mine</a>
+            <a className="control-item">Bookmarks</a>
+          </div>
+
+          <ul className="table-view">
+            <li className="table-view-cell">
+              <a className="navigate-right">
+                Painters
+              </a>
+            </li>
+            <li className="table-view-cell">
+              <a className="navigate-right">
+                Back to the Future
+              </a>
+            </li>
+            <li className="table-view-cell">
+              <a className="navigate-right">
+                Major Wars
+              </a>
+            </li>
+          </ul>
+
+        </div>
+
       </div>
     );
   }
