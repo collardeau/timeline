520785c366b68af5e59d0aaae3de874e801d03a1
Thomas Collardeau <thomas@collardeau.com>
2015-05-27 15:31:07 +0200

    update store

--- app/components/browse/BrowseControls.js
+++ app/components/browse/BrowseControls.js
@@ -5,7 +5,7 @@ class BrowseControls extends React.Component {
   render(){
     return (
       <div className="segmented-control">
-        <a className="control-item active">Only Public</a>
+        <a className="control-item">Only Public</a>
         <a className="control-item">Only Mine</a>
         <a className="control-item">Bookmarks</a>
       </div>
---
+++ app/stores/timelineStore.js
@@ -0,0 +1,60 @@
+const objectAssign = require('react/lib/Object.assign');
+const EventEmitter = require('events').EventEmitter;
+let AppDispatcher = require('../dispatcher/AppDispatcher');
+let appConstants = require('../constants/appConstants');
+let mockupUtils = require('../utils/mockupUtils');
+
+let _store = {
+  timelines: [{
+    name: "Painters",
+    id: "painters"
+  }, {
+    name: "Tonton",
+    id: "tonton"
+  }, {
+    name: "Pearl Jam",
+    id: "pj"
+  }]
+};
+
+const CHANGE_EVENT = 'change';
+
+let timelineStore = objectAssign({}, EventEmitter.prototype, {
+
+  getTimelines() { return _store.timelines; },
+
+  getTimeline(timeline) {
+    console.log("fectching this timeline: ", timeline);
+    return mockupUtils[timeline];
+  },
+
+  getDots() { return _store.timeline.dots; },
+
+  addDot(dot) {
+    _store.dots.push(dot);
+  },
+
+  addChangeListener(cb) { this.on(CHANGE_EVENT, cb); },
+
+  removeChangeListener(cb) { this.removeListener(CHANGE_EVENT, cb); }
+
+});
+
+AppDispatcher.register(function(payload){
+    var action = payload.action;
+    switch(action.actionType){
+        case appConstants.CHANGE_TIMELINE:
+            timelineStore.changeTimeline(action.data.timeline);
+            timelineStore.emit(CHANGE_EVENT);
+            break;
+        default:
+            return true;
+    }
+});
+
+module.exports = timelineStore;
+
+
+
+
+
