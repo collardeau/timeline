6dc1c2c1178a852772f45264c520bf08e6c9bd26
Thomas Collardeau <thomas@collardeau.com>
2015-06-08 23:41:57 +0200

    use promises to create users with unique usernames

--- app/utils/authUtils.js
+++ app/utils/authUtils.js
@@ -4,7 +4,8 @@ let hasher = require('hasher');
 let firebaseUtils = require('./firebaseUtils');
 let userActions = require('../actions/userActions');
 
-let ref = new Firebase(appConstants.FIREBASE_HOST);
+let ref = new Firebase(appConstants.FIREBASE_HOST),
+    usernameRef = ref.child('username');
 
 let saveUsername = (user) => {
   return new Promise(( resolve, reject) => {
@@ -15,13 +16,22 @@ let saveUsername = (user) => {
   });
 };
 
+let removeUsername = (user) => {
+  return new Promise( (resolve, reject) => {
+    usernameRef.child(user.username).set({}, (error) => {
+      if(error) { reject('could not delete username'); }
+      resolve();
+    });
+  });
+};
+
 let createAuthUser = (user) => {
   return new Promise( (resolve, reject) => {
     ref.createUser({
       email: user.email,
       password: user.password
     }, function(error){
-      if(error) { console.log('error'); reject(error.message); }
+      if(error) { reject(error.message); }
       resolve(user);
     });
   });
@@ -55,15 +65,21 @@ let register = (newUser, authData) => {
   });
 };
 
-
 let firebaseAuth = {
 
   createUser: (user, cbOnFail, cbOnSuccess) => {
+
     saveUsername(user)
-    .then(createAuthUser)
-    .then(loginWithPw)
-    .then(register.bind(this, user))
-    .then(cbOnSuccess, cbOnFail);
+    .then(newUser => {
+      createAuthUser(newUser)
+      .then(loginWithPw)
+      .then(register.bind(this, user))
+      .then(cbOnSuccess, function(error) {
+        removeUsername( user )
+        .then(cbOnFail.bind(this, error), console.log);
+      });
+    }, cbOnFail);
+
   },
 
   login: (user, cbOnFail, cbOnSuccess) => {
@@ -78,7 +94,7 @@ let firebaseAuth = {
     return !this.isLoggedIn();
   },
 
-  logout: function(){
+  logout: function(){  // should be passing user callbacks here
     ref.unauth(function(){
       console.log("logged out");
       userActions.changeUser();
