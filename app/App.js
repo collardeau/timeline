54161631cb9362f503a5792249b9827f7dcd6c58
Thomas Collardeau <thomas@collardeau.com>
2015-07-08 14:27:34 +0200

    clean up flux from App.js

--- app/App.js
+++ app/App.js
@@ -5,28 +5,17 @@ const React = require('react');
 const Router = require('react-lil-router');
 const Routes = require('./Routes');
 
-let userActions = require('./actions/userActions');
-let userStore = require('./stores/userStore');
-
 class App extends React.Component {
 
   constructor(){
     super();
     this.state = { 
-      userData: { username: "Billy", bookmarks: [] } 
+      userData: { 
+        username: "Billy", 
+        bookmarks: [] 
+      } 
     };
-    this.changeUserContent = this.changeUserContent.bind(this);
   }
-
-  componentDidMount(){
-    if (this.props.userAuth) {
-      userActions.changeUserData(this.props.userAuth.uid);
-      userActions.changeBookmarkData(this.props.userAuth.uid);
-    } else { console.log('not logged in'); }
-    userStore.addChangeListener(this.changeUserContent);
-  }
-
-  componentWillUnmount(){ userStore.removeChangeListener(this.changeUserContent); }
 
   render() {
     return (
@@ -34,14 +23,6 @@ class App extends React.Component {
         <Routes appState={this.state}/>
       </Router>
     );
-
-  }
-
-  changeUserContent(){
-    console.log("app callback: changing user content");
-    this.setState({
-      userData: userStore.getUserData()
-   });
   }
 
 }
